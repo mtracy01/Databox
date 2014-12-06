7bdf3750197d968b30a4aa5bc27bc998eaf986b2
mtracy01 <matthetracy@gmail.com>
2014-12-06 17:07:35 -0500

    (almost) finished LoginActivity

--- app/src/main/java/com/example/matthew/databox/LoginActivity.java
+++ app/src/main/java/com/example/matthew/databox/LoginActivity.java
@@ -210,13 +210,10 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return email.contains("@") && !email.contains(" ");
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-
         return !password.isEmpty();
     }
 
