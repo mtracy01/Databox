cad00687579793c1bc701ff1383f7941679e35ff
mtracy01 <matthetracy@gmail.com>
2014-12-07 10:59:27 -0500

    Minor updates to loginActivity.java

--- app/src/main/java/com/example/matthew/databox/LoginActivity.java
+++ app/src/main/java/com/example/matthew/databox/LoginActivity.java
@@ -38,7 +38,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
     /**
      * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
+
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
