23822018a2dff16af8b821a89e7967e9c8275f16
mtracy01 <matthetracy@gmail.com>
2014-12-08 10:00:16 -0500

    Minor revisions to LoginActivity

--- app/src/main/java/com/example/matthew/databox/LoginActivity.java
+++ app/src/main/java/com/example/matthew/databox/LoginActivity.java
@@ -35,14 +35,6 @@ import java.util.List;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -137,7 +129,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         }
         else {
             //display error message
-            mEmailView.setError("Invalid Username or Password for Registration");
+            mEmailView.setError("Empty Username or Password for Registration");
             focusView = mPasswordView;
             cancel = true;
             focusView.requestFocus();
